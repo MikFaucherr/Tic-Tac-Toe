4c3806f814e8c73c80e0967ca84741da007d179d
MikFaucher <2042467@etudiant.cegepvicto.ca>
2022-11-13 20:47:12 -0500

    Final

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tableau jeu = new Tableau();
+            bool xGagne = true;
+            bool oGagne = true;
+            char[] tableauposition = new char[9] {
+            '0','1','2',
+            '3','4','5',
+            '6','7','8',
+            };
+            int position = 0;
+            string positionn = "";
+            char symbole = ' ';
+            bool jeufinis = true;
+            int nbtour = 0;
+
+            while (xGagne && oGagne && jeufinis)
+            {
+                //TOUR DES X
+                symbole = 'X';
+                bool positionvalide = true;
+                while (positionvalide)
+                {
+                    Console.WriteLine(tableauposition[0] + " " + tableauposition[1] + " " + tableauposition[2] + "\n" + tableauposition[3] + " " + tableauposition[4] + " " + tableauposition[5] + "\n" + tableauposition[6] + " " + tableauposition[7] + " " + tableauposition[8]);
+                    Console.WriteLine(" C'est au tour du Joueur 1 ou voulez vous mettre votre symbole? ");
+                    positionn = Console.ReadLine();
+                    int.TryParse(positionn, out position);
+                    if (position <= 9)
+                    {
+                        positionvalide = jeu.placerSymbole(position, symbole);
+
+
+                    }
+                }
+
+                    jeu.tableau[position] = symbole;
+                    nbtour++;
+                   
+                    xGagne = jeu.gagnerTicTacToe(symbole);
+
+                    jeufinis = jeu.isFini(nbtour);
+
+                    jeu.afficherJeu();
+
+                //TOUR DES X
+                symbole = 'O';
+                positionvalide = true;
+                while (positionvalide)
+                {
+                    //Console.WriteLine(tableauposition[0] + " " + tableauposition[1] + " " + tableauposition[2] + "\n" + tableauposition[3] + " " + tableauposition[4] + " " + tableauposition[5] + "\n" + tableauposition[6] + " " + tableauposition[7] + " " + tableauposition[8]);
+                    Console.WriteLine(" C'est au tour du Joueur 2 ou voulez vous mettre votre symbole? ");
+                    positionn = Console.ReadLine();
+                    int.TryParse(positionn, out position);
+                    if (position <= 9)
+                    {
+                        positionvalide = jeu.placerSymbole(position, symbole);
+
+
+                    }
+                }
+
+                jeu.tableau[position] = symbole;
+                nbtour++;
+
+                xGagne = jeu.gagnerTicTacToe(symbole);
+
+                jeufinis = jeu.isFini(nbtour);
+
+                jeu.afficherJeu();
+
+
+            }
+        }
+    }
+}
---
+++ Tableau.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+
+    public class Tableau
+    {
+        public char[] tableau;
+        
+
+        public Tableau()
+        {
+            this.tableau = new char[9] {
+            ' ', ' ' , ' ',
+            ' ', ' ', ' ',
+            ' ', ' ', ' ',
+            };
+            
+        }
+
+        public bool gagnerTicTacToe(char symbole)
+        {
+            bool gagne = true;
+
+
+            if (this.tableau[0] == symbole && this.tableau[1] == symbole && this.tableau[2] == symbole)
+            {
+                gagne = false;
+            }
+            if (this.tableau[3] == symbole && this.tableau[4] == symbole && this.tableau[5] == symbole)
+            {
+                gagne = false;
+            }
+        
+            if (this.tableau[6] == symbole && this.tableau[7] == symbole && this.tableau[8] == symbole)
+            {
+                gagne = false;
+        
+        }
+
+            if (this.tableau[0] == symbole && this.tableau[3] == symbole && this.tableau[6] == symbole)
+            {
+                gagne = false;
+        }
+        
+            if (this.tableau[1] == symbole && this.tableau[4] == symbole && this.tableau[7] == symbole)
+            {
+               gagne = false;
+        
+        }
+            if (this.tableau[2] == symbole && this.tableau[5] == symbole && this.tableau[8] == symbole)
+            {
+                gagne = false;
+        
+        }
+            if (this.tableau[0] == symbole && this.tableau[4] == symbole && this.tableau[8] == symbole)
+            {
+                gagne = false;
+        
+        }
+            if (this.tableau[2] == symbole && this.tableau[4] == symbole && this.tableau[6] == symbole)
+            {
+                gagne = false;
+        
+        }
+
+            return gagne;
+        }
+
+        public bool placerSymbole(int position ,  char symbole)
+        {
+            bool placer = true;
+
+            if (this.tableau[position] != 'X' || this.tableau[position] != 'O')
+            {
+            
+                placer = false;  
+            }
+            return placer;
+
+        }
+
+        public bool isFini(int nbtour)
+        {
+            bool isFini = true;
+
+            if (nbtour >= 9)
+            {
+                isFini = false;
+            }
+            return isFini;
+        }
+
+        public void afficherJeu()
+        {
+            Console.WriteLine(this.tableau[0] + " " + this.tableau[1] + " " + this.tableau[2] + "\n" + this.tableau[3] + " " + this.tableau[4] + " " + this.tableau[5] + "\n" + this.tableau[6] + " " + this.tableau[7] + " " + this.tableau[8] );
+        }
+
+    }
+    
+}
